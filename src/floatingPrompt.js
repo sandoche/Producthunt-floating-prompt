7bdfdd67e3fe6ce9c4c658e603e2982938d82c93
Sandoche Adittane <sandoche@protonmail.com>
2019-02-15 19:26:33 +0100

    :construction: Multi modal compatible

--- src/floatingPrompt.js
+++ src/floatingPrompt.js
@@ -1,14 +1,15 @@
 export default class FloatingPrompt {
   constructor(
-    url,
     name,
+    url,
     text = `Hi, do you like ${name} ? Don't forget to show your love on Product Hunt 🚀`,
     buttonText = `${name} on Product Hunt`,
     colorOne = '#da552f',
     colorTwo = '#ea8e39'
   ) {
     /* eslint-disable */
-    this._html = `<div class="producthunt" id="producthunt"> <span class="producthunt__close" id="producthunt-close">×</span><p class="producthunt__text">${text}</p> <a href="${url}" class="button producthunt__button" target="_blank">${buttonText}</a></div>`;
+    this._id = `product-hunt-${name.toLowerCase().replace(/[^a-zA-Z]+/g, "-")}`;
+    this._html = `<div class="producthunt" id="${this._id}"> <span class="producthunt__close" id="${this._id}-close">×</span><p class="producthunt__text">${text}</p> <a href="${url}" class="button producthunt__button" target="_blank">${buttonText}</a></div>`;
     /* eslint-enable */
 
     const prompt = document.createElement('div');
@@ -16,8 +17,8 @@ export default class FloatingPrompt {
     prompt.innerHTML = this._html;
     document.body.appendChild(prompt);
 
-    const producthuntModal = document.getElementById('producthunt');
-    const producthuntCloseButton = document.getElementById('producthunt-close');
+    const producthuntModal = document.getElementById(this._id);
+    const producthuntCloseButton = document.getElementById(`${this._id}-close`);
 
     producthuntCloseButton.addEventListener('click', () => {
       producthuntModal.parentNode.removeChild(producthuntModal);
